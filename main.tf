11805cf69c85a8317dbafcd76f6c84e9beba6e40
Diego Lucía Menéndez <diego.lucia@plainconcepts.com>
2024-10-28 19:06:32 +0100

    repo secrets

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "github_actions_organization_secret" "secrets" {
 
 resource "github_actions_secret" "repository_secrets" {
   count           = length(local.numbers)
-  repository      = "your-repository-name" # Replace with your repository name
+  repository      = "github-universe" # Replace with your repository name
   secret_name     = "SECRET_${local.numbers[count.index]}"
   plaintext_value = local.numbers[count.index]
 }
