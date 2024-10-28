25f6c6b3eaefc20febe7ccc0a2e56b8f49aaba42
Diego Lucía Menéndez <diego.lucia@plainconcepts.com>
2024-10-28 18:57:18 +0100

    variables

--- main.tf
+++ main.tf
@@ -3,9 +3,17 @@ locals {
   numbers = [for i in range(1, 105) : format("%03d", i)]
 }
 
-resource "github_actions_organization_secret" "secrets" {
+resource "github_actions_organization_c" "secrets" {
   count           = length(local.numbers)
   secret_name     = "SECRET_${local.numbers[count.index]}"
   plaintext_value = local.numbers[count.index]
   visibility      = "all"
+}
+
+
+resource "github_organization_variable" "variables" {
+  count      = length(local.numbers)
+  name       = "VARIABLE_${local.numbers[count.index]}"
+  value      = local.numbers[count.index]
+  visibility = "all"
 }
